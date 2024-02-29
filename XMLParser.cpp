dbb9950b49f02a9df43f80ec9849ffa51889f78b
tim <standaert.tim@gmail.com>
2024-02-29 14:11:37 +0100

    initial commit

---
+++ XMLParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tim on 29/02/24.
+//
+
+#include "XMLParser.h"
---
+++ XMLParser.h
@@ -0,0 +1,14 @@
+//
+// Created by tim on 29/02/24.
+//
+
+#ifndef TESTSCONTRACTS_XMLPARSER_H
+#define TESTSCONTRACTS_XMLPARSER_H
+
+
+class XMLParser {
+
+};
+
+
+#endif //TESTSCONTRACTS_XMLPARSER_H
