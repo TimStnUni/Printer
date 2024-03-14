0869e05b66b1872cfc11801bebfa7f98b2b13822
tim <standaert.tim@gmail.com>
2024-03-14 17:11:10 +0100

    requires fixed. creating setters

--- Printer.h
+++ Printer.h
@@ -47,28 +47,63 @@ namespace Printer {
          */
         Device(const Device &inDevice);
 
+
+        /**
+         * \brief Setter function for the name of a device
+         * @param inName string name of device
+         * REQUIRE(!inName.emtpy(), "name should not be empty")
+         * ENSURE(getNameDev() == inName, "name was not correctly set")
+         */
+        void setNameDev(std::string &inName);
+
         /**
          * \brief A getter function for the printer name
          * @return : returns the name of the printer
          * REQUIRE(this->properlyInitialized(), "Device not initialized when calling getNameDev()");
          */
-        std::string getNameDev();
+        std::string getNameDev() const;
 
+
+        /**
+         * \brief A setter function for the printer emissions
+         * @param emissions_in
+         * REQUIRE(emissions_in>0, "emissions should be positive")
+         * ENSURE(this->getEmissions == emissions_in, "Emissions not correctly set")
+         */
+
+        void setEmissions(int &emissions_in);
 
         /**
          * \brief a getter function for the printer emissions
          * @return emissions of the printer in g/page
          * REQUIRE(this->properlyInitialized(), "Device not initialized when calling getEmissions()");
          */
-        int getEmissions();
+        int getEmissions() const;
+
+
+        /**
+         * \brief A setter function for the printer speed
+         * @param speed_in
+         * REQUIRE(speed_in > 0, "Speed should be positive")
+         * ENSURE(this->getSpeed() == speed_in, "Speed wasn't correctly set")
+         */
+
+        void setSpeed(int &speed_in);
 
         /**
          * \brief a getter function for the printer speed
          * @return speed in pages/minute
          * REQUIRE(this->properlyInitialized(), "Device not initialized when calling getSpeed()")
          */
-        int getSpeed();
+        int getSpeed() const;
 
+
+        bool operator == (const Device &d){
+            if (name == d.name && emissions == d.emissions && speed == d.speed){
+                return true;
+            }
+            return false;
+        }
 
     protected:
         std::string name;
@@ -77,7 +112,7 @@ namespace Printer {
         Device *_initCheck;
 
 
-        bool properlyInitialized();
+        bool properlyInitialized() const;
 
 
     };
@@ -96,6 +131,9 @@ namespace Printer {
          * @param pageCount_in How many pages need to be printed
          * @param jobNr_in JobNr for the job
          *
+         *REQUIRE(!userName_in.empty(), "Username shouldn't be empty");
+         *REQUIRE(pageCount_in>0, "Pagecount should be positive");
+         *
          * ENSURE(this->properlyInitialized(), "Job wasn't properly initialized")
          */
         Job(std::string userName_in, int pageCount_in, int jobNr_in);
@@ -106,29 +144,56 @@ namespace Printer {
          * @return unsigned integer version of the jobnr, these should theoretically always be positive, though this is not required by the standard
          * REQUIRE(this->properlyInitialized(), "Job wasn't initialized when calling getJobNr()")
          */
-        unsigned int getJobNr();
+        unsigned int getJobNr()const;
 
         /**
          * \brief Getter function for the Username
          * @return username as a string
          * REQUIRE(this->properlyInitialized(), "Job wasn't initialized when calling getUserName()")
          */
-        std::string getUserName();
+        std::string getUserName()const;
 
         /**
          * \brief Getter function for the PageCount
          * @return
          * REQUIRE(this->properlyInitialized(), "Job wasn't initialized when calling getPageCount()")
          */
-        int getPageCount();
+        int getPageCount()const;
 
+
+        /**
+         * \brief Setter function for jobNr
+         * @param jobNr
+         * REQUIRE(jobNr_in>0, "jobnr should be positive")
+         * ENSURE(this->getJobNr() == jobNr_in, "Jobnr not correctly set")
+         */
+
+        void setJobNr(int jobNr);
+
+        /**
+         * \brief Setter function for the pagecount
+         * @param pageCount
+         * REQUIRE(pageCount_in > 0, "Pagecount should be positive")
+         * ENSURE(this->getPageCount() == pageCount_in, "Pagecount not correctly set")
+         */
+
+        void setPageCount(int pageCount);
+
+
+        /**
+         * \brief Setter function for username
+         * @param userName
+         * REQUIRE(!userName_in.empty(), "Username should not be empty")
+         * ENSURE(this->getUserName() == userName_in, "Username not correctly set")
+         */
+        void setUserName(const std::string &userName);
 
     private:
         int jobNr, pageCount;
         std::string userName;
         Job *_initCheck;
 
-        bool properlyInitialized();
+        bool properlyInitialized() const;
 
 
     };
@@ -156,46 +221,50 @@ namespace Printer {
          */
         XMLParser(const char *filename);
 
-        /**
-         * \brief Constructor for the parser which takes a string. Currently not implemented
-         * @param filename : Name of file to parse as a string
-         */
-        //XMLParser(std::string filename);
+        void addInputFile(const char * filename);
 
         ~XMLParser();
 
 
-
-       /*
-
-         * \brief Getter function for the name. Not properly defined at the moment. XMLParser need to go through a thorough redesign
+        /**
+         * \brief Getter function that returns all Devices read in from xml
          * @return
-         * REQUIRE(this->properlyInitialized(), "Parser not properly initialized when calling getNameDev()")
-
-        std::string getName();
-
-
-        //REQUIRE(this->properlyInitialized(), "Parser not properly initialized when calling getSpeed()");
-        int getSpeed();
-
-
-        //REQUIRE(this->properlyInitialized(), "Parser not properly initialized when calling getEmissions()");
-        int getEmissions();
-
-        */
-        //REQUIRE(this->properlyInitialized(), "Parser not properly initialized when calling getDeviceList()");
+         * REQUIRE(this->properlyInitialized, "XMLParser wasn't properly initialized)
+         */
         std::vector<Device> getDeviceList();
 
 
-        //REQUIRE(this->properlyInitialized(), "Parser not properly initialized when calling getJobList()")
+        /**
+         * \brief Getter function for getting a joblist
+         * @return double ended queue of jobs
+         * REQUIRE(this->properlyInitialized(), "Parser not properly initialized when calling getJobList()")
+         */
+
+
         std::deque<Job> getJobList();
 
-        //REQUIRE(this->properlyInitialized(), "Parser not properly initialized when calling getJobNrList()")
+
+        /**
+         * \brief Getter function for the map of jobNr's to jobs
+         * @return
+         * //REQUIRE(this->properlyInitialized(), "Parser not properly initialized when calling getJobNrList()")
+         */
+
         std::map<unsigned int, unsigned int> getJobNrMap();
+
+        /**
+         * \brief Getter function for the set of jobnr's
+         * @return
+         * REQUIRE(this->properlyInitialized(), "parser not properly initialized when calling getJobNrSet()");
+         */
 
         std::set<unsigned int> getJobNrSet();
 
 
+        /**
+         * \brief Function that returns the amount of jobs. Currently unused
+         * @return
+         */
         //REQUIRE(this->properlyInitialized(), "Parser not properly initialized when calling getNrOfJobs()")
         int getNrOfJobs();
 
@@ -206,19 +275,16 @@ namespace Printer {
         bool properlyInitialized();
 
 
+        /**
+         * \brief Function that parses the XML file supplied to the constructor.
+         */
+
         void parse();
 
         TiXmlDocument InputDoc;
 
 
-        //these should probably all be removed
-        /*
-        std::string name;
-        int speed, emissions;
 
-        int jobNr, pageCount;
-        std::string userName;
-*/
 
         //new datamembers
         std::vector<Device> deviceList;
@@ -258,7 +324,7 @@ namespace Printer {
          * \brief Function to add a printer device to a printer container
          * @param device_in a vector of devices to be added.
          * REQUIRE(this->properlyInitialized(), "Printer was not initialized when calling addDevices()")
-         * ENSURE(getDevice(device_in.getNameDev) == device_in, "adding devices went wrong
+         *
          * //The above ensure is not yet implemented, and wouldn't work with the current implementation anyways
          */
         void addDevices(std::vector<Device> device_in);
@@ -269,9 +335,9 @@ namespace Printer {
          * @param jobnrs Unordered set of jobnrs. This is in the process of being reimplemented
          * REQUIRE(this->properlyInitialized(), "Printer was not initialized when calling addJobs()")
          */
-        void addJobs(std::deque<Job> &jobs, std::map<unsigned int, unsigned int> &jobnrs, std::set<unsigned int> &jobnrSet);
-        //Todo: Change unordered set into something better (probably just a vector here)
-        //Also potential to have this be a map of jobnrs to jobs. This keeps the duplication checking intact
+        void
+        addJobs(std::deque<Job> &jobs, std::map<unsigned int, unsigned int> &jobnrs, std::set<unsigned int> &jobnrSet);
+
 
 
         //this is technically not very general
@@ -280,7 +346,7 @@ namespace Printer {
          * @return first device stored in the printer
          * REQUIRE(this->properlyInitialized(), "Printer was not initialized when calling getDevice()")
          */
-        Device getDevice();
+        Device getDevice(int index);
 
         /**
          * \brief Getter for all jobs
@@ -300,6 +366,7 @@ namespace Printer {
 
 
         void removeJob(unsigned int jobNr);
+
     private:
 
         Printer *_initCheck;
