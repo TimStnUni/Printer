79cb49860ad53c367592495bcc649481b3bb78f0
Jasmine2607 <159305849+Jasmine2607@users.noreply.github.com>
2024-03-17 20:50:29 +0100

    testen en debugging

--- tests.cpp
+++ tests.cpp
@@ -1,57 +1,23 @@
 //
-// Created by tim on 29/02/24.
+// Created by alyja on 14/03/2024.
 //
 
-#include "gtest/gtest.h"
+
+#include <iostream>
+#include <fstream>
+#include <sys/stat.h>
+#include <gtest/gtest.h>
+
+using namespace std;
 #include "Printer.h"
-#include "JobTesten/JobInputTests.cpp"
+#include "./PrinterTesten/PrinterDomainTests.cpp"
+#include "./PrinterTesten/PrinterOutputTests.cpp"
+#include "./PrinterTesten/PrinterInputTests.cpp"
+#include "./JobTesten/JobInputTests.cpp"
+#include "./JobTesten/JobOutputTests.cpp"
 
-
-using namespace System;
-
-class XMLTest: public ::testing::Test {
-protected:
-    // You should make the members protected s.t. they can be
-    // accessed from sub-classes.
-
-    // virtual void SetUp() will be called before each test is run.  You
-    // should define it if you need to initialize the variables.
-    // Otherwise, this can be skipped.
-    virtual void SetUp() {
-    }
-
-    // virtual void TearDown() will be called after each test is run.
-    // You should define it if there is cleanup work to do.  Otherwise,
-    // you don't have to provide it.
-    virtual void TearDown() {
-    }
-
-    // Declares the variables your tests want to use.
-    XMLParser ppp_;
-};
-
-
-//TEST_F(XMLTest, AfterParse){
-//    XMLParser testParser("test.xml");
-//
-//    EXPECT_EQ(testParser.getJobList().size(), 2);
-//    EXPECT_EQ(testParser.getJobList().begin()->getJobNr(), 89751);
-//
-//
-//}
-
-
-int main(int argc, char ** argv){
-
+int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-
-
-
-
-
-    return 0;
 }
-
-
 
