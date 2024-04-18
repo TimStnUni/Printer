697be7b71a6dd47506e724d9490ae006db498880
Jasmine2607 <159305849+Jasmine2607@users.noreply.github.com>
2024-04-18 14:24:53 +0200

    testen en debugging + Thomas aanpassingen

--- PrinterTesten/tests.cpp
+++ PrinterTesten/tests.cpp
@@ -1,7 +1,10 @@
-//
-// Created by alyja on 14/03/2024.
-//
-
+//============================================================================
+// Name        : tests.cpp
+// Author      : Tim Standaert, Jasmine Castillo, Thomas D'Archambeau
+// Date        : 29/02/2024
+// Version     : 17++
+// Description : PrinterSystem in C++, Ansi-style
+//============================================================================
 
 #include <iostream>
 #include <fstream>
@@ -15,10 +18,8 @@ using namespace std;
 #include "./PrinterTesten/PrinterInputTests.cpp"
 #include "./JobTesten/JobInputTests.cpp"
 #include "./JobTesten/JobOutputTests.cpp"
-#include "./JobTesten/JobDomainTests.cpp"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
